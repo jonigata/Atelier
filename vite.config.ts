111090718ba22fe10b89193caebfbcbc19969b3f
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2026-03-05 00:21:16 +0900

    tailscale対応

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 5473
-	}
+		port: 5473,
+	    allowedHosts: ['ai.tail21014d.ts.net'],
+	},
 });
